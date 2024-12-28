8d6c7f8801896f390f06760cd9de5a954bc7da5f
Rowdy Rabouw <hi@rowdy.codes>
2024-12-28 14:27:30 +0100

    feat: write to file from firestore

--- firestore/getAll.mjs
+++ firestore/getAll.mjs
@@ -1,7 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { collection, getDocs, getFirestore, terminate } from "firebase/firestore";
+import { promisify } from "util";
+import fs from "fs";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+
 dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -15,10 +25,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+console.log(">> Fetching data from Firestore");
 const querySnapshot = await getDocs(collection(db, "countries"));
+
+console.log(`>> Writing ${querySnapshot.size} countries to json file`);
+const countries = [];
 querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  const country = { id: doc.id, ...doc.data() };
+  countries.push(country);
 });
-console.log("Countries:", querySnapshot.size);
+await writeFile(`src/_data/countries.json`, JSON.stringify(countries), (err) => {
+  if (err) throw err;
+});
+
+console.log(`>> Writing ${countries.length} countries to javascript file`);
+const countriesJsObject = countries.reduce((acc, country) => {
+  acc[country.placeId] = parseInt(country.visitedBy);
+  return acc;
+}, {});
+await writeFile(`src/assets/js/countries.js`, "export const countries =" + JSON.stringify(countriesJsObject) + ";", (err) => {
+  if (err) throw err;
+});
 
 terminate(db);
