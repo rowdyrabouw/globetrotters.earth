07f15af662932b180315a813b73728a349165058
Rowdy Rabouw <hi@rowdy.codes>
2024-12-29 17:17:21 +0100

    fix: fix server

--- server/server.mjs
+++ server/server.mjs
@@ -20,7 +20,7 @@ app.listen(3000, () => {
   console.log(">> Admin server running on port 3000");
 });
 
-app.use((res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
