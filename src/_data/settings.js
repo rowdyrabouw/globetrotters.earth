f4a98cecfc8702c5ea45f444c0318d743ae44773
Rowdy Rabouw <hi@rowdy.codes>
2024-12-29 16:55:40 +0100

    feat: add firestore env

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -1,8 +1,14 @@
 export default () => {
   console.log(">> Running settings.js");
   return {
-    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-    environment: process.env.NODE_ENV || "PRD",
     now: new Date(),
+    environment: process.env.NODE_ENV || "PRD",
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    firestoreApiKey: process.env.FIREBASE_API_KEY,
+    firestoreAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firestoreProjectId: process.env.FIREBASE_PROJECT_ID,
+    firestoreStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firestoreMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firestoreAppId: process.env.FIREBASE_APP_ID,
   };
 };
